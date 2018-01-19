cde2a4e72e8ebf0061d0316763301f093dae0527
Emre <EmreOvunc@users.noreply.github.com>
2018-01-19 13:39:39 +0300

    Update Log-Analysis.py

--- Log-Analysis.py
+++ Log-Analysis.py
@@ -4,41 +4,57 @@
 import os
 
 # General Paths & Variables
-webIP = 'YOUR WEB SERVER IP'
-logPath = '/var/log/'
+webIP       = 'YOUR SERVER IP ADDRESS(IPV4)'
+logPath     = '/var/log/'
 tmpIptables = '/tmp/tmp_iptables.txt'
 
 # Auth Statics
-auth = 'auth'
-logAuth = logPath + 'auth.log'
-tmpAuth = '/tmp/tmp_authList.txt'
-grepAuth = " grep 'sshd.*fail' | grep -ho 'rhost=\w*.\w*.\w*.\w*'"
-cutAuth = " cut -c 7-"
+auth        = 'auth'
+logAuth     = logPath + 'auth.log'
+tmpAuth     = '/tmp/tmp_authList.txt'
+grepAuth    = " grep 'sshd.*fail' | grep -ho 'rhost=\w*.\w*.\w*.\w*'"
+cutAuth     = " cut -c 7-"
 blacklist_Auth = 'blacklist_Auth.txt'
 
 # Apache Statics
-apache = 'apache'
-apachePath = logPath + 'apache2/'
-accessLog = apachePath + 'access.log'
+apache      = 'apache'
+apachePath  = logPath + 'apache2/'
+accessLog   = apachePath + 'access.log'
 blacklist_Apache = 'blacklist_Apache.txt'
-grepApache1 = " grep -E 'scan|script|prompt|bash|admin|root|command|manager|login|sql|db|database|myadmin|mysql|administrator|pma|PMA|setup'"
+grepApache1 = "grep -E 'scan|script|prompt|bash|admin|root|command|manager|login|sql|db|database|myadmin|mysql" \
+              "|administrator|pma|PMA|setup|=+UNION+ALL+select+NULL+--+|AND+1%3D2+--+|OR+1%3D1+--+|response.write%28100%2C000*100%2C000%29" \
+              "|%27%3Bprint%28chr%28122%29.chr%2897%29|cat+%2Fetc%2Fpasswd%26'"
 grepApache2 = " grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'"
-tmpApache = "/tmp/tmp_apacheList.txt"
+tmpApache   = "/tmp/tmp_apacheList.txt"
+
+# FTP Statics
+ftp           = 'vsftpd'
+ftpPath       = logPath + ftp + '.log'
+tmpFtp        = '/tmp/tmp_ftpList.txt'
+grepFtp       = " grep 'FAIL LOGIN' | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'"
+blacklist_Ftp = 'blacklist_Ftp.txt'
 
 # Mail Statics
-apache_header = 'Virtual Ubuntu WEB Attacks'
-auth_header = 'Virtual Ubuntu SSH Attempts'
-receiver_mail = " YOUR MAIL ADDRESS"
+apache_header   = 'Virtual Server WEB Attacks'
+auth_header     = 'Virtual Server SSH Attempts'
+ftp_header      = 'Virtual Server FTP Attacks'
+receiver_mail   = " " + "YOUR MAIL ADDRESS"
+
 
 def create_black_lists():
     if os.path.exists(tmpApache):
         os.system('rm -rf ' + tmpApache)
     if os.path.exists(tmpAuth):
         os.system('rm -rf ' + tmpAuth)
+    if os.path.exists(tmpFtp):
+        os.system('rm -rf ' + tmpFtp)
     if not os.path.exists(blacklist_Apache):
         os.system('touch ' + blacklist_Apache)
     if not os.path.exists(blacklist_Auth):
         os.system('touch ' + blacklist_Auth)
+    if not os.path.exists(blacklist_Ftp):
+        os.system('touch ' + blacklist_Ftp)
+
 
 def add_iptables(IP):
     if "\n" in IP:
@@ -46,23 +62,6 @@ def add_iptables(IP):
     command = 'iptables -A INPUT -s ' + str(IP) + ' -j DROP'
     os.system(command)
 
-def check_other_IPs():
-    os.system("iptables -S > " + tmpIptables)
-    iptablesFile = open(tmpIptables, 'r+')
-    iptablesLines = iptablesFile.readlines()
-    authFile = open(blacklist_Auth, 'r+')
-    apacheFile = open(blacklist_Apache, 'r+')
-    for rules in iptablesLines:
-        for IPs in authFile:
-            if not IPs == '\n':
-                rule = '-A INPUT -s ' + str(IPs)
-                if rule not in rules:
-                    add_iptables(str(IPs))
-        for IPss in apacheFile:
-            if not IPss == '\n':
-                rule = '-A INPUT -s ' + str(IPss)
-                if rule not in rules:
-                    add_iptables(str(IPss))
 
 def add_black_lists(typeLog, IP):
     if typeLog == auth:
@@ -74,6 +73,12 @@ def add_black_lists(typeLog, IP):
         apache_blacklist = open(blacklist_Apache, 'a')
         apache_blacklist.write("\n"+str(IP))
         apache_blacklist.close()
+
+    elif typeLog == ftp:
+        ftp_blacklist = open(blacklist_Ftp, 'a')
+        ftp_blacklist.write("\n"+str(IP))
+        ftp_blacklist.close()
+
 
 def send_mail(typeLog, IP, count):
     if typeLog == auth:
@@ -87,6 +92,13 @@ def send_mail(typeLog, IP, count):
         content = "echo 'Someone is trying web attacks ! [" + IP + "] tried and BLOCKED :)'"
         mail_fail = str(content + mail_data)
         os.system(mail_fail)
+
+    elif typeLog == ftp:
+        mail_data = " | mail -s '" + ftp_header + "' " + receiver_mail
+        content = "echo 'Someone is trying ftp attacks ! [" + IP + "] tried and BLOCKED :)'"
+        mail_fail = str(content + mail_data)
+        os.system(mail_fail)
+
 
 def eliminate_duplicates(typeLog, file_name):
     wordcount = {}
@@ -112,6 +124,15 @@ def eliminate_duplicates(typeLog, file_name):
                     add_iptables(str(k))
                     send_mail(apache, str(k), 0)
 
+    elif typeLog == ftp:
+        for k, v in wordcount.items():
+            if str(k) != webIP:
+                if not check_same_ip(ftp, str(k)):
+                    add_black_lists(ftp, str(k))
+                    add_iptables(str(k))
+                    send_mail(ftp, str(k), 0)
+
+
 def check_same_ip(typeLog, IP):
     if typeLog == auth:
         auth_blacklist = open(blacklist_Auth, 'r+')
@@ -129,11 +150,21 @@ def check_same_ip(typeLog, IP):
         apache_blacklist.close()
         return False
 
+    elif typeLog == ftp:
+        ftp_blacklist = open(blacklist_Ftp, 'r+')
+        for IPs in ftp_blacklist:
+            if IP in IPs:
+                return True
+        ftp_blacklist.close()
+        return False
+
+
 def apache_log():
     os.system("cat " + accessLog + "|" + grepApache1 + "|" + grepApache2 + " > " + tmpApache)
     tmpApache_File = open(tmpApache, 'r+')
     eliminate_duplicates(apache, tmpApache_File)
     tmpApache_File.close()
+
 
 def auth_log():
     os.system("cat " + logAuth + "|" + grepAuth + "|" + cutAuth + " > " + tmpAuth)
@@ -141,12 +172,34 @@ def auth_log():
     eliminate_duplicates(auth, tmpAuth_File)
     tmpAuth_File.close()
 
+
+def vsftp_log():
+    os.system("cat " + ftpPath + "|" + grepFtp + " > " + tmpFtp)
+    tmpFtp_File = open(tmpFtp, 'r+')
+    eliminate_duplicates(ftp, tmpFtp_File)
+    tmpFtp_File.close()
+
+
 def main():
     create_black_lists()
-    auth_log()
-    apache_log()
-    check_other_IPs()
+
+    try:
+        auth_log()
+    except:
+        pass
+
+    try:
+        apache_log()
+    except:
+        pass
+
+    try:
+        vsftp_log()
+    except:
+        pass
+
     os.system('iptables-save')
+
 
 if __name__ == "__main__":
     main()
